819506dfed8d053c0f9a25c82bc4a0c4770cb05a
HuynhManDat <JuniorKevin110@gmail.com>
2021-12-02 03:49:50 +0700

    Init Source

---
+++ EFCoreCodeFirst/EFCoreCodeFirst/Entities/ChiTietDonHang.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirst.Entities
+{
+    [Table("ChiTietDonHang")]
+    public class ChiTietDonHang
+    {
+        [Key]
+        public Guid MaCtDh { get; set; }
+        public Guid MaDH { get; set; }
+        [ForeignKey("MaDH")]
+        public DonHang DonHang { get; set; }
+
+        public Guid MaHh { get; set; }
+        [ForeignKey("MaHh")]
+        public HangHoa HangHoa { get; set; }
+
+        public int SoLuong { get; set; }
+    }
+}
---
+++ EFCoreCodeFirst/EFCoreCodeFirst/Migrations/20211201194800_Them_Don_Hang.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreCodeFirst.Migrations
+{
+    public partial class Them_Don_Hang : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DonGia",
+                table: "HangHoa",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.CreateTable(
+                name: "DonHang",
+                columns: table => new
+                {
+                    MaDH = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NgayDat = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NguoiDat = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DonHang", x => x.MaDH);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChiTietDonHang",
+                columns: table => new
+                {
+                    MaCtDh = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MaDH = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MaHh = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SoLuong = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChiTietDonHang", x => x.MaCtDh);
+                    table.ForeignKey(
+                        name: "FK_ChiTietDonHang_DonHang_MaDH",
+                        column: x => x.MaDH,
+                        principalTable: "DonHang",
+                        principalColumn: "MaDH",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ChiTietDonHang_HangHoa_MaHh",
+                        column: x => x.MaHh,
+                        principalTable: "HangHoa",
+                        principalColumn: "MaHh",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChiTietDonHang_MaDH",
+                table: "ChiTietDonHang",
+                column: "MaDH");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChiTietDonHang_MaHh",
+                table: "ChiTietDonHang",
+                column: "MaHh");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChiTietDonHang");
+
+            migrationBuilder.DropTable(
+                name: "DonHang");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "DonGia",
+                table: "HangHoa",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
