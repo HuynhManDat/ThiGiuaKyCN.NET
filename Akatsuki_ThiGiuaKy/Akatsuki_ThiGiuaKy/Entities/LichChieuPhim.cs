0b6c45ddae41db8cfb1dd75cc94bc1c3996e1a7a
ngdanhtrung <nhoxprokute03@gmail.com>
2021-12-02 08:17:31 +0700

    added LichChieuPhim.cs

--- Akatsuki_ThiGiuaKy/Akatsuki_ThiGiuaKy/Entities/LichChieuPhim.cs
+++ Akatsuki_ThiGiuaKy/Akatsuki_ThiGiuaKy/Entities/LichChieuPhim.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,26 @@ namespace Akatsuki_ThiGiuaKy.Entities
 {
     public class LichChieuPhim
     {
+        [Key]
+        public int MaLichChieuPhim { get; set; }
+
+        [ForeignKey(nameof(Rap))]
+        [Display(Name = "Rap")]
+        public int MaRap { get; set; }
+
+        public Rap Rap { get; set; }
+
+        [ForeignKey(nameof(Phim))]
+        [Display(Name = "Phim")]
+        public int MaPhim { get; set; }
+
+        public Phim Phim { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Thời gian chiếu phim")]
+        public DateTime ThoiGianChieu { get; set; }
+
+        public ICollection<DatVe> DatVes { get; set; }
     }
 }
