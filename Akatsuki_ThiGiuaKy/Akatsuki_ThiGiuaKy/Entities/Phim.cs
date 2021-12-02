33f63a679ae3de1b5c787bf0df2fea75c32b71a3
HuynhManDat <JuniorKevin110@gmail.com>
2021-12-02 08:12:59 +0700

    Push Phim, Rap Entities

---
+++ Akatsuki_ThiGiuaKy/Akatsuki_ThiGiuaKy/Entities/Phim.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akatsuki_ThiGiuaKy.Entities
+{
+    public class Phim
+    {
+        [Key]
+        public int MaPhim { get; set; }
+
+        [Required]
+        [Display(Name = "Tên Phim")]
+        public string TenPhim { get; set; }
+
+        [Display(Name = "Thời Lượng")]
+        public int ThoiLuong { get; set; }
+        public ICollection<LichChieuPhim> LichChieuPhims { get; set; }
+    }
+}
---
+++ Akatsuki_ThiGiuaKy/Akatsuki_ThiGiuaKy/Entities/Rap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akatsuki_ThiGiuaKy.Entities
+{
+    public class Rap
+    {
+        [Key]
+        public int MaRap { get; set; }
+
+        [Display(Name = "Tên Rạp")]
+        public string TenRap { get; set; }
+
+        [Display(Name = "Số Chỗ Trống")]
+        public int SoChoTrong { get; set; }
+        public ICollection<LichChieuPhim> LichChieuPhims { get; set; }
+    }
+}
