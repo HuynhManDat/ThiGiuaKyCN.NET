55c62cede75dead15298049a64c959305a213ff5
HuynhManDat <JuniorKevin110@gmail.com>
2021-12-02 08:13:39 +0700

    Update

---
+++ Akatsuki_ThiGiuaKy/Akatsuki_ThiGiuaKy/Entities/LichChieuPhim.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akatsuki_ThiGiuaKy.Entities
+{
+    public class LichChieuPhim
+    {
+    }
+}
