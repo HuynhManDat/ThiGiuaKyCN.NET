c58c7b4f21aad75b0e2b67d8258299e2aaa79e72
HuynhManDat <JuniorKevin110@gmail.com>
2021-12-02 08:25:06 +0700

    Add DatVe and migration

---
+++ Akatsuki_ThiGiuaKy/Akatsuki_ThiGiuaKy/Entities/DatVe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akatsuki_ThiGiuaKy.Entities
+{
+    public class DatVe
+    {
+        [Key]
+        public int MaDatVe { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Số điện thoại người xem")]
+        public string SoDienThoai { get; set; }
+
+        [ForeignKey(nameof(LichChieuPhim))]
+        [Display(Name = "Lịch chiếu")]
+        public int MaLichChieuPhim { get; set; }
+
+        [Range(1, Int32.MaxValue)]
+        [Display(Name = "Số lượng đặt")]
+        public int SoLuong { get; set; } = 1;
+
+        public LichChieuPhim LichChieuPhim { get; set; }
+
+        [Display(Name = "Vé được đặt lúc")]
+        public DateTime ThoiGianDat { get; set; }
+    }
+}
