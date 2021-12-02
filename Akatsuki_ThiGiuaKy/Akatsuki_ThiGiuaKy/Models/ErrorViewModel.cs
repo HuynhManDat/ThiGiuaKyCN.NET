608ee36a0dfd169d01e73f33486dee04523ea0af
HuynhManDat <JuniorKevin110@gmail.com>
2021-12-02 07:47:46 +0700

    Init Source

---
+++ Akatsuki_ThiGiuaKy/Akatsuki_ThiGiuaKy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Akatsuki_ThiGiuaKy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
